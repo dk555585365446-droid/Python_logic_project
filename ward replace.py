876c260c39bc9579b1605ff40696f7ee2c87e8ff
dk555585365446-droid <dk555585365446@gmail.com>
2026-05-26 08:37:51 +0530

    Create ward replace.py

---
+++ ward replace.py
@@ -0,0 +1,8 @@
+# Word replace
+user = input("enter your story : ")
+
+while True :
+  user1= input("enter your bad  : ")
+  user2 = input("enter your new wrod : ")
+  b = user.replace(user1 , user2)
+  print(b)
