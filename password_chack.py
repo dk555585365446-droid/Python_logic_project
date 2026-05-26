ebc13f509af01610b811f4274d8ab8517ccccb69
dk555585365446-droid <dk555585365446@gmail.com>
2026-05-26 08:07:36 +0530

    Create password_chack.py

---
+++ password_chack.py
@@ -0,0 +1,11 @@
+user = input("enter your name : ")
+use2= input("enter your passwrod : ")
+name = "Deepak"
+passwrod  = "Deepa90"
+
+if name == user and use2 == passwrod :
+  print("login ssecful")
+  print("wllcom", user)
+
+else:
+  print("dont passwrod")
