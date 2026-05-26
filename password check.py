129e9a7691133cad2fa104581ffb617797ef708a
dk555585365446-droid <dk555585365446@gmail.com>
2026-05-26 08:19:34 +0530

    Create password check.py

---
+++ password check.py
@@ -0,0 +1,15 @@
+while True :
+  user = input("enter your name : ")
+  use2= input(" your passwrod : ")
+  name = "Deepak"
+  passwrod  = "Deepa90"
+  if name == user and use2 == passwrod :
+    k= "login ssecful"
+    print(k)
+    print("wllcom", user)
+    if  k == "login ssecful":
+      print("wllocm")
+      break
+  else:
+    print("dont passwrod")
+  
