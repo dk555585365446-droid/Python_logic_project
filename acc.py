0d989454456ec9e5aa033e86d9456b8c5e1adc2f
dk555585365446-droid <dk555585365446@gmail.com>
2026-06-05 22:35:10 +0530

    Create acc.py

---
+++ acc.py
@@ -0,0 +1,11 @@
+student = {
+  "name":"Deepak",
+  "age" : 16,
+  "mark" : 362,
+  "Callass" : "12th",
+  "mother" : "jasobanti pradhan ",
+  "father" : "pradip prdhan"
+}
+user = input("enter your keys : ")
+b = student[user]
+print("value :",b)
