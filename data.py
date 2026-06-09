a747c31e71b2a16a52bc136855c14cf177120067
dk555585365446-droid <dk555585365446@gmail.com>
2026-06-09 07:58:43 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,12 @@
+# data add
+data = {1, 2, 3, 4, 5, 6, 7,}
+iser = int(input(" how many data  "))
+for i in range (iser):
+  lser2 = int(input(" how many remove  "))
+  lser1 = int(input(" how many data  "))
+  data.remove(lser2)
+  data.add(lser1)
+print(data)
+
+  
+  
